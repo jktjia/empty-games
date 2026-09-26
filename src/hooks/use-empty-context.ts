e424ecc12fc9b35bc3129d19577c8eb51a793a53
jktjia <jamiekai.san@gmail.com>
2026-09-26 13:06:00 -0400

    chore: cleaned up empty context hook

--- src/hooks/use-empty-context.ts
+++ src/hooks/use-empty-context.ts
@@ -18,15 +18,13 @@ export function useEmptyProvider({
     return localTime ? new Date(JSON.parse(localTime)) : new Date()
   })
 
-  // const [tooLong, setTooLong] = useState<number>(-1)
   const [showMessage, setShowMessage] = useState<boolean>(false)
   const [pokes, setPokes] = useState<number>(0)
 
-  const [abandoned, setAbandoned] = useState<number>(0)
   const [timeoutId, setTimeoutId] = useState<NodeJS.Timeout>()
 
   const [interfereAllowed, setInterfereAllowed] = useState<boolean>(true)
-  const [ignoreCount, setIgnoreCount] = useState<number>(0)
+  // const [ignoreCount, setIgnoreCount] = useState<number>(0)
 
   const startTime = new Date()
 
@@ -37,53 +35,8 @@ export function useEmptyProvider({
   const updateActivity = useCallback(() => {
     const now = new Date()
     setLastActivity(now)
-    setAbandoned(0)
     updateLocal(now)
-  }, [setLastActivity, setAbandoned, timeoutId, setTimeoutId])
-
-  const afkMessage = useCallback(() => {
-    const now = new Date()
-    const diff = now.valueOf() - lastActivity.valueOf()
-    console.log('last activity: %d minutes ago', diff / (60 * 1000))
-    if (diff < 5 * 60 * 1000 * timeoutModifier) {
-      const id = setTimeout(
-        afkMessage,
-        (5 * 60 * 1000 - diff) * timeoutModifier,
-      )
-      setTimeoutId(id)
-      console.log('timeout reset')
-    } else {
-      // setAbandoned((a) => {
-      //     const message = abandonedMessages[Math.min(a, abandonedMessages.length - 1)]
-      //     toast(message.title, {
-      //         description: message.description
-      //     })
-      //     toast('Where did you go?', {
-      //         description: `last activity: ${Math.floor(diff / (60 * 1000))} minutes, ${Math.floor((diff / 1000) % 60)} seconds ago`
-      //     })
-      //     return a + 1
-      // })
-      const time =
-        abandoned < 4
-          ? 60 * 1000 + Math.floor(Math.random() * 240 * 1000)
-          : abandoned < 8
-            ? 240 * 1000 + Math.floor(Math.random() * 360 * 1000)
-            : 600 * 1000 + Math.floor(Math.random() * 1200 * 1000)
-      const id = setTimeout(afkMessage, time * timeoutModifier)
-      setTimeoutId(id)
-      console.log('timeout set')
-    }
-  }, [setAbandoned])
-
-  // const poke = useCallback(() => {
-  //   const now = new Date()
-  //   const startDiff = now.valueOf() - startTime.valueOf()
-  //   const lastDiff = now.valueOf() - lastActivity.valueOf()
-  //   // setTooLong(Math.floor(startDiff / (5 * 60 * 1000 * timeoutModifier)))
-  //   setShowMessage(true)
-  //   setTimeout(() => setShowMessage(false), 2.5 * 1000 * timeoutModifier)
-  //   setTimeout(poke, 5 * 60 * 1000 * timeoutModifier)
-  // }, [startTime, lastActivity, timeoutModifier, setShowMessage])
+  }, [setLastActivity, timeoutId, setTimeoutId])
 
   useEffect(() => {
     const now = new Date()
@@ -98,21 +51,13 @@ export function useEmptyProvider({
       2 * 60 * 1000 * timeoutModifier,
     )
     return () => clearTimeout(timeout)
-  }, [pokes])
+  }, [setShowMessage, pokes])
 
   useEffect(() => {
     const startDiff = startTime.valueOf() - lastActivity.valueOf()
     if (startDiff > 20 * 60 * 1000 * timeoutModifier) {
       navigate({ to: '/' + DONT_LEAVE_PATH })
     }
-    // if (!timeoutId) {
-    //   const id = setTimeout(afkMessage, 300 * 1000 * timeoutModifier)
-    //   setTimeoutId(id)
-    //   console.log('timeout set')
-    //   setTimeout(poke, 5 * 60 * 1000 * timeoutModifier)
-    // } else {
-    //   console.log('timeout not set')
-    // }
   }, [])
 
   const wheatMessage = useMemo(
